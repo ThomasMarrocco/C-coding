cdc70735d93e611135c591c49cdaa15916cf8bb9
ThomasMarrocco <98425366+ThomasMarrocco@users.noreply.github.com>
2022-01-31 21:46:51 +0000

    Create intro.c

---
+++ intro.c
@@ -0,0 +1,26 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <math.h>
+
+int main(void)
+{
+    int a = 4;
+    int b = 5;
+    int c = b/a;
+ printf("%i \n", c);
+}
+
+
+
+
+//int is a data type, stands for integer//
+//   \n - new line //
+
+//errors://
+//implicit decleration of function-typo//
+//always make before run//
+//save befpre make//
