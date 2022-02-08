f2757596e2efd9da1fc9d58a9b51ccdcebbd767e
ThomasMarrocco <98425366+ThomasMarrocco@users.noreply.github.com>
2022-02-08 20:47:08 +0000

    Create Terminal navigation.c

---
+++ Terminal navigation.c
@@ -0,0 +1,13 @@
+~/test/ $ cd ..
+~/ $ cd ..
+/home/ $ pwd
+/home
+/home/ $ cd ununtu
+bash: cd: ununtu: No such file or directory
+/home/ $ cd ubuntu
+~/ $ ls
+test/
+~/ $ cd test
+~/test/ $ ls
+can*  can.c
+~/test/ $ 
