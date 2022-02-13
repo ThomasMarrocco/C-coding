a91ad5ce28c2b563ec0375c20477325cce15acaf
Thomas Marrocco <98425366+ThomasMarrocco@users.noreply.github.com>
2022-02-13 13:49:10 +0000

    Create else.c

---
+++ if/else.c
@@ -0,0 +1,17 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <math.h>
+
+int main(void)
+{
+ char bacon = 'c';
+ if(bacon == 'b'){printf("yes");}
+ else{printf("Not B, it is: %c" , bacon);}
+ 
+}
+
+
