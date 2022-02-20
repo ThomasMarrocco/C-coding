003f41b4d0ac7903c4cd6a58584e0b7c7c241790
Thomas Marrocco <98425366+ThomasMarrocco@users.noreply.github.com>
2022-02-20 22:14:42 +0000

    Update Pointers.c

--- Pointers.c
+++ Pointers.c
@@ -1,1 +1,40 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <math.h>
 
+int main(void)
+{
+ int n = 5;
+
+printf("%i \n" , n);
+
+printf("%p" , &n);
+}
+
+
+
+
+
+//Running://
+//- ls to find file//
+//- cd to correct folder//
+//- make "name of file"//
+//- ./"name of file"//
+
+//printing://
+//integers:  printf("%i" , variable name); //
+//pointers:  printf("%p" , &variable name);//
+// for new line - ("%i \n", variable name); //
+
+//int is a data type, stands for integer//
+//only state data type once, not needed in printing //
+
+//errors://
+//implicit decleration of function-typo//
+//always make before run//
+//save befpre make//
+// number to left of error show line and character of mistake//
